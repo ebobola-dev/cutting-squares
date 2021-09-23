1558a84e035a9cd6fb5a0542e2c496c4628ab4f7
ebobola-web <romawot94@gmail.com>
2021-09-23 17:26:48 +0300

    home

--- cutting_squares/Source.cpp
+++ cutting_squares/Source.cpp
@@ -4,21 +4,41 @@
 int main() {
 	setlocale(LC_ALL, "Rus");
 
-	int a, b, c, kol, res, count = 0;
-	printf("Введите a, b: ");
-	scanf("%d%d", &a, &b);
+	int a, b, c, kol, res, cont, count = 0;
 
-	while (1) {
-		if (a < b) { c = a; a = b; b = c; }
-		kol = a / b;
-		res = a % b;
-		printf("\nОтрезали квадрат со стороной %d %d раз(а)", b, kol);
-		a = b;
-		b = res;
-		count++;
-		if (b == 0) {
-			printf("\nКонец. Всего отрезали %d раз\n", count);
-			break;
+	while (1){
+		printf("Введите a, b: ");
+		scanf("%d%d", &a, &b);
+
+		while (1) {
+			if (a > 0 && b > 0) break;
+			printf("\nНужны положительные значения!\n");
+			if (a <= 0) {
+				printf("Введите a ещё раз: ");
+				scanf("%d", &a);
+			}
+			if (b <= 0) {
+				printf("Введите b ещё раз: ");
+				scanf("%d", &b);
+			}
 		}
+
+		while (1) {
+			if (a < b) { c = a; a = b; b = c; }
+			kol = a / b;
+			res = a % b;
+			printf("\nОтрезали квадрат %dx%d | %d раз(а)", b, b, kol);
+			a = b;
+			b = res;
+			count++;
+			if (b == 0) {
+				printf("\nКонец. Всего отрезали %d раз\n", count);
+				break;
+			}
+		}
+		printf("\nВведите 0, чтобы закончить, 1 - продолжить: ");
+		scanf("%d", &cont);
+		if (!cont) break;
+		else printf("\n");
 	}
 }
